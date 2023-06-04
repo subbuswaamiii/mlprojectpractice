c325bf8fe44756b72f00f8b868ecf7f87312772c
subbuswaamiii <subbuswaamiii@gmail.com>
2023-06-04 17:53:28 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+
+name = 'mlproject',
+version = '0.0.1',
+author = 'Subbu',
+author_email = 'subbuswaamiii@gmail.com',
+packages = find_packages(),
+install_requires = ['pandas', 'numpy', 'seaborn']
+
+
+)
